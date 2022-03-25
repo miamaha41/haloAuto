32f845b599c501f73d16fd2e5a7151e8abce8213
Nguyen Thi Hai Yen <miamaha41@gmail.com>
2022-03-25 17:27:40 +0700

    product routes

---
+++ server/src/controllers/product.js
@@ -0,0 +1,80 @@
+import Product from "../models/Product.js";
+import products from "../utilities/product.js";
+
+export const createProducts = async (req, res) => {
+  try {
+    if (Product) {
+      await Product.deleteMany({});
+    }
+    const importProducts = await Product.insertMany(products);
+    res.send({ importProducts });
+  } catch (error) {
+    console.error(error);
+    res.send({ message: error.message });
+  }
+};
+
+export const insertedProduct = async (req, res) => {
+  const product = req.body.product;
+  if (!product) {
+    res.send({ message: "Must have product!" });
+  }
+  try {
+    const insertProduct = new Product(product);
+    await insertProduct.save();
+
+    res.send({ insertProduct, message: "Product was inserted!" });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+};
+
+export const getProducts = async (req, res) => {
+  try {
+    const listProduct = await Product.find();
+    res.send({ listProduct });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+};
+
+export const deletedProduct = async (req, res) => {
+  const productId = req.params.id;
+  console.log(productId);
+  if (!productId) {
+    res.send({ message: "Must have product Id!" });
+  }
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(productId);
+    if (!deleteProduct) {
+      return res.status(401).json({
+        success: false,
+        message: "Product not found !",
+      });
+    }
+    res.send({ deleteProduct, message: "Product was deleted!" });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+};
+
+export const updatedProduct = async (req, res) => {
+  let product = req.body.product;
+  const productId = req.params.id;
+  if (!product || !productId) {
+    res.send({ message: "Must have product , product ID!" });
+  }
+  try {
+    let updateProduct = await Product.findOneAndUpdate(
+      { _id: productId },
+      product,
+      {
+        new: true,
+      }
+    );
+    if (updateProduct.error) res.send({ message: error.message });
+    res.send({ updateProduct, message: "Product was updated!" });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+};
