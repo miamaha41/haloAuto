d207b2d1ec3f9f26119af5b0a5af738ca2498dc2
Nguyen Thi Hai Yen <miamaha41@gmail.com>
2022-03-26 10:04:23 +0700

    fix error cors

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import productRouter from "./routes/v1/productRouter.js";
 import orderRouter from "./routes/v1/orderRouter.js";
 import { connectDB } from "./config/mongoDb.js";
-
+import cors from "cors";
 // connectDB()
 //   .then(() => console.log("Connected successfully to database server!"))
 //   .then(() => bootServer())
@@ -16,6 +16,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors());
 app.use("/product", productRouter);
 app.use("/order", orderRouter);
 app.listen(PORT, () => {
